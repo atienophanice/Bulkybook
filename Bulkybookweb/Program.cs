55126aec3f6fe8b42100e9da4a25df7e875ce05d
phanice owino <owinophanice54@gmail.com>
2022-11-11 11:05:30 +0300

    added   builder that will attomatically find the connection string from app.jesson

--- Bulkybookweb/Program.cs
+++ Bulkybookweb/Program.cs
@@ -1,10 +1,14 @@
 using Bulkybookweb.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer);
+builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+});
 
 var app = builder.Build();
 
