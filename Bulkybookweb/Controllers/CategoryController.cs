540aa502139560ba6ec3fac260250391658b49c0
phanice owino <owinophanice54@gmail.com>
2022-11-11 12:29:23 +0300

    created a link for the project to retrieve data of the category databse to the project

--- Bulkybookweb/Controllers/CategoryController.cs
+++ Bulkybookweb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulkybookweb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bulkybookweb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+           var objCategoryList=_db.Categories.ToList();
             return View();
         }
     }
