793fefcacd3416779988c1fe8f1e130387739e6a
phanice owino <owinophanice54@gmail.com>
2022-11-11 11:25:13 +0300

    add a migration to the database to make changes to it

--- Bulkybookweb/Data/ApplicationDbContext.cs
+++ Bulkybookweb/Data/ApplicationDbContext.cs
@@ -3,12 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Bulkybookweb.Data
 {
-public class ApplicationDbContext:DbContext
-{
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-  
+    public class ApplicationDbContext : DbContext
     {
-    }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+
+        {
+        }
         public DbSet<Category> Categories { get; set; }
 
- }
+    }
+}
