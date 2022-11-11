598a066f720225fabba254adf0aaa622ec5149e4
phanice owino <owinophanice54@gmail.com>
2022-11-11 10:26:18 +0300

    Added the name of the database tables as categories

--- Bulkybookweb/Data/ApplicationDbContext.cs
+++ Bulkybookweb/Data/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bulkybookweb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bulkybookweb.Data
 {
-    public class ApplicationDbContext:DbContext
+public class ApplicationDbContext:DbContext
+{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+  
     {
     }
-}
+        public DbSet<Category> Categories { get; set; }
+
+ }
